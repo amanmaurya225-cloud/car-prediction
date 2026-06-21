f50ac49f10a15833bf6ed6603a9bb42aa4626811
Aman Maurya <amanmaurya5g2025@gmail.com>
2026-06-21 15:40:00 +0530

    Add CORS middleware to FastAPI application

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 from schema import CarFeature , PredictionResponse
 from model import load_artifacts , predict_price
 from fastapi.responses import JSONResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(title="Car Price Prediction API", version="1,0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.on_event("startup")
 def startup_event():
